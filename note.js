32c850b86a9ad20ecbadb60779d11c05490c59c1
Jatin <jatin.kshatriya2821@gmail.com>
2021-07-16 08:34:27 +0530

    Working Title

--- note.js
+++ note.js
@@ -2,66 +2,73 @@ var addBtn = document.getElementById('addNote');
 showNotes() 
 
 addBtn.addEventListener('click', () => {
-    // let noteTitle = document.getElementById('noteTitle');
+    let noteTitle = document.getElementById('noteTitle');
     let note = document.getElementById('noteText');
     let storage = localStorage.getItem('storage');
-    // console.log(noteTitle.value);
+
+    showMsg();
 
     if (storage == null) {
-        // titleObj = [];
-        noteObj = [];
+        noteCont={
+            'titleArr' : [],
+            'noteArr' : []
+       };
     }
 
     else {
-        // titleObj = JSON.parse(noteTitle);
-        noteObj = JSON.parse(storage);
+        noteCont = JSON.parse(storage);
     }
-
-    // titleObj.push(noteTitle.value);
-    noteObj.push(note.value);
-    localStorage.setItem("storage", JSON.stringify(noteObj));
-    note.value = '';
+    if (note.value!='' || noteTitle.value!=''){
+    noteCont.titleArr.push(noteTitle.value);
+    noteCont.noteArr.push(note.value);
+    localStorage.setItem("storage", JSON.stringify(noteCont));
+    note.value='';
+    noteTitle.value='';
     showNotes();
+    }
 })
 
 function showNotes() {
     let storage = localStorage.getItem('storage');
-   
-    
-    if (storage == null) {
-        // titleObj = [];
-        noteObj = [];
-    }
-    else {
-        // titleObj = JSON.parse(noteTitle);
-        noteObj = JSON.parse(storage);
-    }
-    localStorage.setItem("storage", JSON.stringify(noteObj));
-    html = `<h5 class="my-2">You can see or delete your notes here</h5>`
-    
-    noteObj.forEach(function(element, index){
+    let html = ` `;
 
+    if (storage == null) {
+        noteCont={
+            'titleArr' : [],
+            'noteArr' : []
+       };
+    }   
+    else {
+        noteCont = JSON.parse(storage);
+    }  
+    localStorage.setItem("storage", JSON.stringify(noteCont));
+
+    noteCont.titleArr.forEach(function(element, index){
+        const nelement = noteCont.noteArr[index]
         html += `
         <div class="col-sm-4 my-2" id="${index}">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">element${index}</h5>
-                    <p class="card-text">${element}</p>
-                    <button type="button" id="${index}" onclick=deleteNote(this.id) class="btn btn-primary deleteBtn">Delete Note</button>
+                    <h5 class="card-title">${element}</h5>
+                    <p class="card-text">${nelement}</p>
+                    <button type="button" id="${index}"  onclick=deleteNote(this.id) class="btn btn-danger">Delete Note</button>
                 </div>
             </div>
         </div>              
         `;
 
     });
+
+    
     let userNotes = document.getElementById('userNotes');
     
     if(!storage){
-        localStorage.setItem("storage", JSON.stringify(noteObj));
+        localStorage.setItem("storage", JSON.stringify(noteCont));
     }
 
-    if (storage.length == 0) {
-        userNotes.innerHTML = html;
+    if (storage.length == 28) {
+        html = `<h5 class="my-2">You can see or delete your notes here</h5>`;
+        console.log("check")
     }
     if (storage.length != 0) {
         userNotes.innerHTML = html;
@@ -72,9 +79,26 @@ function showNotes() {
 function deleteNote(index){
 
     document.getElementById(index).remove();
-    noteObj.splice(index, 1);
-    localStorage.setItem('storage', JSON.stringify(noteObj));
+    noteCont.titleArr.splice(index, 1);
+    noteCont.noteArr.splice(index, 1);
+    localStorage.setItem('storage', JSON.stringify(noteCont));
     console.log('removed', index)
     showNotes();
 }
 
+function showMsg(){
+    let noteTitle = document.getElementById('noteTitle');
+    let note = document.getElementById('noteText');
+    let msgval = document.getElementById('warningmsg');
+    let msg = ``;
+    if (note.value=='' || noteTitle.value==''){
+        msg = ` 
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+            <strong>Empty Fields!</strong> 
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `
+    };
+    msgval.innerHTML=msg;
+}
